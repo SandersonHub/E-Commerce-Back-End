1455467b1c06850936942469003ca5ff9d8260bd
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-08-07 11:02:43 -0400

    revamped tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,20 +18,68 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+
+
+//GET route for tag of ID
+
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagInfo = await Tag.findByPk(req.params.id, { //finding all tags by their id
+      include: [{ model: Product, through: ProductTag, as: 'productTag_products'}]
+    });
+    //model: Product, tells sequelize to return the product model
+    //through: ProductTag, tells sequelize to include any product tags
+    //as: 'productTag_products' tells sequelize to return the product tag model as productTag_products
 
+//if respond not found catch this error
+    if (!tagInfo) {
+      res.status(404).json({ message: 'tag didnt work' });
+    } else {
+      res.status(200).json(tagInfo);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+//POST route for new tag
 
 router.post('/', async (req, res) => {
-  // create a new tag
+  try {
+    const newTag = await Tag.create(req.body); //creates a new tag in the db, await is used to wait for 
+    //the db operation to complete before moving to the next line
 
+    //error checking, return 201 if successful, catch error display status 500.
+    res.status(201).json(newTag); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
-router.delete('/:id', async (req, res) => {
+//DELETE route for tag of ID
+
+router.delete('/:id', async (req, res) => { //handling http delete request
   // delete on tag by its `id` value
+  
+  //try / catch block
+  try { 
+    const tagToDelete = await Tag.findByPk(req.params.id); //finds a tag by its id
+
+
+    //if respond not found catch this error
+    if (!tagToDelete) {
+      res.status(404).json({ message: 'Tag didnt work' }); //if tag not found, return 404
+    } else {
+      await tagToDelete.destroy(); //deletes the tag
+      res.status(200).json({ message: 'Tag deleted'}); //returns status 200, successful. 
+    }
+  } catch (err) { //catches any errors, returns status 500
+    res.status(500).json(err);
+  }
 
 
 });
