15783d20bdc64fff04577d59d498a4dc9360edc2
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-08-07 11:46:51 -0400

    updating models/index.js still researching....

--- models/index.js
+++ models/index.js
@@ -5,19 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
 
+});
 // Categories have many Products
 
+
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
 
+});
+
+
+
+
+
+
+
+
+
+//exporting the four variables
 module.exports = {
   Product,
   Category,
   Tag,
   ProductTag,
 };
-
-
-//still need to figure this one out
